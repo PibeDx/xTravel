3747602605d2b4355a08b99b55a6f95e0c2f5373
b9007 <alejandro.bautista90@yahoo.com.mx>
2016-06-09 14:40:10 -0500

    updating

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,35 @@
 /**
  * Created by abautista on 09/06/2016.
  */
+'use strict'
+
+var app = angular.module("IniciaWebApp", [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'firebase'
+])
+
+    .constant('FBURL','https://project-6381816712702858280.firebaseio.com/')
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/',{
+                templateURL: 'vistas/principal.html'
+            })
+            .when('/cocina',{
+                templateURL: 'vistas/cocina.html'
+            })
+            .when('/entrepreneurship',{
+                templateURL: 'vistas/entrepreneurship.html'
+            })
+            .when('/idiomas',{
+                templateURL: 'vistas/idiomas.html'
+            })
+            .when('/programacion',{
+                templateURL: 'vistas/programacion.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    
+});
